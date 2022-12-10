106e68ccaeda3a42e889547b40bde81f0d4f1251
evalsium2 <117542711+evalsium2@users.noreply.github.com>
2022-12-10 21:19:37 +0300

    Add files via upload

---
+++ colorbase.py
@@ -0,0 +1,354 @@
+# red
+IndianRed = 205, 92, 92
+LightCoral = 240, 128, 128
+Salmon = 250, 128, 114
+DarkSalmon = 233, 150, 122
+LightSalmon1 = 255, 160, 122
+Crimson = 220, 20, 60
+Red = 255, 0, 0
+FireBrick = 178, 34, 34
+DarkRed = 139, 0, 0
+
+red = [IndianRed, LightCoral, Salmon, DarkSalmon, LightSalmon1, Crimson, Red, FireBrick, DarkRed]
+
+# pink
+Pink = 255, 192, 203
+LightPink = 255, 182, 193
+HotPink = 255, 105, 180
+DeepPink = 255, 20, 147
+MediumVioletRed = 199, 21, 133
+PaleVioletRed = 219, 112, 147
+
+pink = [Pink, LightPink, HotPink, DeepPink, MediumVioletRed, PaleVioletRed]
+
+# orange
+LightSalmon2 = 255, 160, 122
+Coral = 255, 127, 80
+Tomato = 255, 99, 71
+OrangeRed = 255, 69, 0
+DarkOrange = 255, 140, 0
+Orange = 255, 165, 0
+
+orange = [LightSalmon2, Coral, Tomato, OrangeRed, DarkOrange, Orange]
+
+# yellow
+Gold = 255, 215, 0
+Yellow = 255, 255, 0
+LightYellow = 255, 255, 224
+LemonChiffon = 255, 250, 205
+LightGoldenrodYellow = 250, 250, 210
+PapayaWhip = 255, 239, 213
+Moccasin = 255, 228, 181
+PeachPuff = 255, 218, 185
+PaleGoldenrod = 238, 232, 170
+Khaki = 240, 230, 140
+DarkKhaki = 189, 183, 107
+
+yellow = [Gold, Yellow, LightYellow, LemonChiffon, LightGoldenrodYellow, PapayaWhip, Moccasin, PeachPuff, PaleGoldenrod,
+          Khaki, DarkKhaki]
+
+# purple
+Lavender = 230, 230, 250
+Thistle = 216, 191, 216
+Plum = 221, 160, 221
+Violet = 238, 130, 238
+Orchid = 218, 112, 214
+Fuchsia = 255, 0, 255
+Magenta = 255, 0, 255
+MediumOrchid = 186, 85, 211
+MediumPurple = 147, 112, 219
+BlueViolet = 138, 43, 226
+DarkViolet = 148, 0, 211
+DarkOrchid = 153, 50, 204
+DarkMagenta = 139, 0, 139
+Purple = 128, 0, 128
+Indigo = 75, 0, 130
+SlateBlue = 106, 90, 205
+DarkSlateBlue = 72, 61, 139
+
+purple = [Orchid, Fuchsia, Magenta, MediumOrchid, MediumPurple, BlueViolet, DarkViolet, DarkOrchid, DarkMagenta, Purple,
+          Indigo, SlateBlue, DarkSlateBlue]
+
+# brown
+Cornsilk = 255, 248, 220
+BlanchedAlmond = 255, 235, 205
+Bisque = 255, 228, 196
+NavajoWhite = 255, 222, 173
+Wheat = 245, 222, 179
+BurlyWood = 222, 184, 135
+Tan = 210, 180, 140
+RosyBrown = 188, 143, 143
+SandyBrown = 244, 164, 96
+Goldenrod = 218, 165, 32
+DarkGoldenRod = 184, 134, 11
+Peru = 205, 133, 63
+Chocolate = 210, 105, 30
+SaddleBrown = 139, 69, 19
+Sienna = 160, 82, 45
+Brown = 165, 42, 42
+Maroon = 128, 0, 0
+
+brown = [Cornsilk, BlanchedAlmond, Bisque, NavajoWhite, Wheat, BurlyWood, Tan, RosyBrown, SandyBrown, Goldenrod,
+         DarkGoldenRod, Peru, Chocolate, SaddleBrown, Sienna, Brown, Maroon]
+
+# green
+GreenYellow = 173, 255, 47
+Chartreuse = 127, 255, 0
+LawnGreen = 124, 252, 0
+Lime = 0, 255, 0
+LimeGreen = 50, 205, 50
+PaleGreen = 152, 251, 152
+LightGreen = 144, 238, 144
+MediumSpringGreen = 0, 250, 154
+SpringGreen = 0, 255, 127
+MediumSeaGreen = 60, 179, 113
+SeaGreen = 46, 139, 87
+ForestGreen = 34, 139, 34
+Green = 0, 128, 0
+DarkGreen = 0, 100, 0
+YellowGreen = 154, 205, 50
+OliveDrab = 107, 142, 35
+Olive = 128, 128, 0
+DarkOliveGreen = 85, 107, 47
+MediumAquamarine = 102, 205, 170
+DarkSeaGreen = 143, 188, 143
+LightSeaGreen = 32, 178, 170
+DarkCyan = 0, 139, 139
+Teal = 0, 128, 128
+
+green = [GreenYellow, Chartreuse, LawnGreen, Lime, LimeGreen, PaleGreen, LightGreen, MediumSpringGreen, SpringGreen,
+         MediumSeaGreen, SeaGreen, ForestGreen, Green, DarkGreen, YellowGreen, OliveDrab, Olive, DarkOliveGreen,
+         MediumAquamarine, DarkSeaGreen, LightSeaGreen, DarkCyan, Teal]
+
+# blue
+Aqua = 0, 255, 255
+Cyan = 0, 255, 255
+LightCyan = 224, 255, 255
+PaleTurquoise = 175, 238, 238
+Aquamarine = 127, 255, 212
+Turquoise = 64, 224, 208
+MediumTurquoise = 72, 209, 204
+DarkTurquoise = 0, 206, 209
+CadetBlue = 95, 158, 160
+SteelBlue = 70, 130, 180
+LightSteelBlue = 176, 196, 222
+PowderBlue = 176, 224, 230
+LightBlue = 173, 216, 230
+SkyBlue = 135, 206, 235
+LightSkyBlue = 135, 206, 250
+DeepSkyBlue = 0, 191, 255
+DodgerBlue = 30, 144, 255
+CornflowerBlue = 100, 149, 237
+MediumSlateBlue = 123, 104, 238
+RoyalBlue = 65, 105, 225
+Blue = 0, 0, 255
+MediumBlue = 0, 0, 205
+DarkBlue = 0, 0, 139
+Navy = 0, 0, 128
+MidnightBlue = 25, 25, 112
+
+# white
+White = 255, 255, 255
+Snow = 255, 250, 250
+Honeydew = 240, 255, 240
+MintCream = 245, 255, 250
+Azure = 240, 255, 255
+AliceBlue = 240, 248, 255
+GhostWhite = 248, 248, 255
+WhiteSmoke = 245, 245, 245
+Seashell = 255, 245, 238
+Beige = 245, 245, 220
+OldLace = 253, 245, 230
+FloralWhite = 255, 250, 240
+Ivory = 255, 255, 240
+AntiqueWhite = 250, 235, 215
+Linen = 250, 240, 230
+LavenderBlush = 255, 240, 245
+MistyRose = 255, 228, 225
+
+# gray
+Gainsboro = 220, 220, 220
+LightGrey = 211, 211, 211
+LightGray = 211, 211, 211
+Silver = 192, 192, 192
+DarkGray = 169, 169, 169
+DarkGrey = 169, 169, 169
+Gray = 128, 128, 128
+Grey = 128, 128, 128
+DimGray = 105, 105, 105
+DimGrey = 105, 105, 105
+LightSlateGray = 119, 136, 153
+LightSlateGrey = 119, 136, 153
+SlateGray = 112, 128, 144
+SlateGrey = 112, 128, 144
+DarkSlateGray = 47, 79, 79
+DarkSlateGrey = 47, 79, 79
+Black = 0, 0, 0
+
+# red
+INDIANRED = (205, 92, 92)
+LIGHTCORAL = (240, 128, 128)
+SALMON = (250, 128, 114)
+DARKSALMON = (233, 150, 122)
+LIGHTSALMON1 = (255, 160, 122)
+CRIMSON = (220, 20, 60)
+RED = (255, 0, 0)
+FIREBRICK = (178, 34, 34)
+DARKRED = (139, 0, 0)
+
+# pink
+PINK = (255, 192, 203)
+LIGHTPINK = (255, 182, 193)
+HOTPINK = (255, 105, 180)
+DEEPPINK = (255, 20, 147)
+MEDIUMVIOLETRED = (199, 21, 133)
+PALEVIOLETRED = (219, 112, 147)
+
+# orange
+LIGHTSALMON2 = (255, 160, 122)
+CORAL = (255, 127, 80)
+TOMATO = (255, 99, 71)
+ORANGERED = (255, 69, 0)
+DARKORANGE = (255, 140, 0)
+ORANGE = (255, 165, 0)
+
+# yellow
+GOLD = (255, 215, 0)
+YELLOW = (255, 255, 0)
+LIGHTYELLOW = (255, 255, 224)
+LEMONCHIFFON = (255, 250, 205)
+LIGHTGOLDENRODYELLOW = (250, 250, 210)
+PAPAYAWHIP = (255, 239, 213)
+MOCCASIN = (255, 228, 181)
+PEACHPUFF = (255, 218, 185)
+PALEGOLDENROD = (238, 232, 170)
+KHAKI = (240, 230, 140)
+DARKKHAKI = (189, 183, 107)
+
+# purple
+LAVENDER = (230, 230, 250)
+THISTLE = (216, 191, 216)
+PLUM = (221, 160, 221)
+VIOLET = (238, 130, 238)
+ORCHID = (218, 112, 214)
+FUCHSIA = (255, 0, 255)
+MAGENTA = (255, 0, 255)
+MEDIUMORCHID = (186, 85, 211)
+MEDIUMPURPLE = (147, 112, 219)
+BLUEVIOLET = (138, 43, 226)
+DARKVIOLET = (148, 0, 211)
+DARKORCHID = (153, 50, 204)
+DARKMAGENTA = (139, 0, 139)
+PURPLE = (128, 0, 128)
+INDIGO = (75, 0, 130)
+SLATEBLUE = (106, 90, 205)
+DARKSLATEBLUE = (72, 61, 139)
+
+# brown
+CORNSILK = (255, 248, 220)
+BLANCHEDALMOND = (255, 235, 205)
+BISQUE = (255, 228, 196)
+NAVAJOWHITE = (255, 222, 173)
+WHEAT = (245, 222, 179)
+BURLYWOOD = (222, 184, 135)
+TAN = (210, 180, 140)
+ROSYBROWN = (188, 143, 143)
+SANDYBROWN = (244, 164, 96)
+GOLDENROD = (218, 165, 32)
+DARKGOLDENROD = (184, 134, 11)
+PERU = (205, 133, 63)
+CHOCOLATE = (210, 105, 30)
+SADDLEBROWN = (139, 69, 19)
+SIENNA = (160, 82, 45)
+BROWN = (165, 42, 42)
+MAROON = (128, 0, 0)
+
+# green
+GREENYELLOW = (173, 255, 47)
+CHARTREUSE = (127, 255, 0)
+LAWNGREEN = (124, 252, 0)
+LIME = (0, 255, 0)
+LIMEGREEN = (50, 205, 50)
+PALEGREEN = (152, 251, 152)
+LIGHTGREEN = (144, 238, 144)
+MEDIUMSORINGGREEN = (0, 250, 154)
+SPRINGGREEN = (0, 255, 127)
+MEDIUMSEAGREEN = (60, 179, 113)
+SEAGREEN = (46, 139, 87)
+FORESTGREEN = (34, 139, 34)
+GREEN = (0, 128, 0)
+DARKGREEN = (0, 100, 0)
+YELLOWGREEN = (154, 205, 50)
+OLIVEDRAB = (107, 142, 35)
+OLIVE = (128, 128, 0)
+DARKOLIVEGREEN = (85, 107, 47)
+MEDIUMAQUAMARINE = (102, 205, 170)
+DARKSEAGREEN = (143, 188, 143)
+LIGHTSEAGREEN = (32, 178, 170)
+DARKCYAN = (0, 139, 139)
+TEAL = (0, 128, 128)
+
+# blue
+AQUA = (0, 255, 255)
+CYAN = (0, 255, 255)
+LIGHTCYAN = (224, 255, 255)
+PALETURQUOISE = (175, 238, 238)
+AQUAMARINE = (127, 255, 212)
+TURQUOISE = (64, 224, 208)
+MEDIUMTURQUOISE = (72, 209, 204)
+DARKTURQUOISE = (0, 206, 209)
+CADETBLUE = (95, 158, 160)
+STEELBLUE = (70, 130, 180)
+LIGHTSTEELBLUE = (176, 196, 222)
+POWDERBLUE = (176, 224, 230)
+LIGHTBLUE = (173, 216, 230)
+SKYBLUE = (135, 206, 235)
+LIGHTSKYBLUE = (135, 206, 250)
+DEEPSKYBLUE = (0, 191, 255)
+DODGERBLUE = (30, 144, 255)
+CORNFLOWERBLUE = (100, 149, 237)
+MEDIUMSLATEBLUE = (123, 104, 238)
+ROYALBLUE = (65, 105, 225)
+BLUE = (0, 0, 255)
+MEDIUMBLUE = (0, 0, 205)
+DARKBLUE = (0, 0, 139)
+NAVY = (0, 0, 128)
+MIDNIGHTBLUE = (25, 25, 112)
+
+# white
+WHITE = (255, 255, 255)
+SNOW = (255, 250, 250)
+HONEYDEW = (240, 255, 240)
+MINTCREAM = (245, 255, 250)
+AZURE = (240, 255, 255)
+ALICEBLUE = (240, 248, 255)
+GHOSTWHITE = (248, 248, 255)
+WHITESMOKE = (245, 245, 245)
+SEASHELL = (255, 245, 238)
+DEIGE = (245, 245, 220)
+OLDLACE = (253, 245, 230)
+FLORALWHITE = (255, 250, 240)
+IVORY = (255, 255, 240)
+ANTIQUEWHITE = (250, 235, 215)
+LINEN = (250, 240, 230)
+LAVENDERBLUSH = (255, 240, 245)
+MISTYROSE = (255, 228, 225)
+
+# gray
+GAINSBORO = (220, 220, 220)
+LIGHTGREY = (211, 211, 211)
+LIGHTGRAY = (211, 211, 211)
+SILVER = (192, 192, 192)
+DARKGRAY = (169, 169, 169)
+DARKGREY = (169, 169, 169)
+GRAY = (128, 128, 128)
+GREY = (128, 128, 128)
+DIMGRAY = (105, 105, 105)
+DIMGREY = (105, 105, 105)
+LIGHTSLAYGRAY = (119, 136, 153)
+LIGHTSLAYGREY = (119, 136, 153)
+SLATEGRAY = (112, 128, 144)
+SLATEGREY = (112, 128, 144)
+DARKSLATEGRAY = (47, 79, 79)
+DARKSLATEGREY = (47, 79, 79)
+BLACK = (0, 0, 0)
